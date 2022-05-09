f341dc41923fe78b349425106afc88c46543636d
Davide Cesari <dcesari@arpae.it>
2022-05-09 18:10:43 +0200

    many untested modifications

--- src/fsm.py
+++ src/fsm.py
@@ -1,7 +1,5 @@
 #!/usr/bin/micropython
 
-state = byterray(4) # auto trig, man, out timer, out
-
 states = [
     "at1_man1_osf1", "at0_man1_osf1", "at1_man0_osf1", "at0_man0_osf1",
     "at1_man1_osf0", "at0_man1_osf0", "at1_man0_osf0", "at0_man0_osf0",
@@ -12,51 +10,137 @@ states = [
 # events = setman, setnoman, onman, offman, autotrigrise, autotrigdrop,
 #  ontimer, ontimerend, offtimer, offtimerend
 
-def setman(self):
-    self.state[1] = 1
+class KnovaTool:
+    def getstate(self, req, qs):
+        return state
 
-def setnoman(self):
-    self.state[1] = 0
-    self.state[2] = 0
-    self.state[3] = self.state[0]
+class DigitalOut(KnovaTool):
+    def __init__(self, conf):
+        self.name = conf["name"]
+        self.invert = conf.get("invert", False)
+        self.web = conf.get("web", False)
+        self.pin = conf["pin"]
+        self.defaultstate = conf.get(conf["defaultstate"], 0)
 
-def onman(self):
-    self.state[2] = 0
-    self.state[3] = 1
+        self.state = bytearr(0)
+        self.state[0] = self.defaultstate
 
-def offman(self):
-    self.state[2] = 0
-    self.state[3] = 0
+    def connect(self, unitlist):
+        if self.web:
+            unitlist["web"].register((self.name,"get"), self.getstate)
 
-def autotrigrise(self):
-    self.state[0] = 1
-    if self.state[1] == 0:
-        self.state[3] = 1
+    def setoutput(self, state):
+        if self.invert: self.state[0] = 1 - state
+        else: self.state[0] = state
+        # machine.pin(self.pin, self.state[0])~
 
-def autotrigdrop(self):
-    self.state[0] = 0
-    if self.state[1] == 0:
-        self.state[3] = 0
+            
+class Switch:
+    def __init__(self, conf):
+        self.name = conf["name"]
+        self.inputs = conf.get("inputs", [])
+        self.output = conf["output"]
+        self.inputop = conf.get("inputop", "or")
+        self.web = conf.get("web", False)
+        self.timerdeflen = conf.get("timerdeflen", 60)
+        self.defaultstate = conf.get(conf["defaultstate"], 0)
 
-def ontimer(self):
-    self.state[2] = 1
-    self.state[3] = 1
+        if not self.web and len(inputs) == 0:
+            raise "no inputs"
 
-def ontimerend(self):
-    self.state[2] = 0
-    if self.state[1] == 0:
+        self.state = byterray(4) # auto trig, man, out timer, out
+        if len(inputs) > 0: self.state[1] = 0 # automatic
+        else: self.state[1] = 1 # manual
+        self.state[2] = 0 # output by timer off
+        self.state[3] = 0 # output off
+
+    def connect(self, unitlist):
+        self.ins = []
+        for inp in self.inputs:
+            self.ins.append(unitlist["inp"])
+        self.out = unitlist["out"]
+        if self.web:
+            unitlist["web"].register((self.name,"set","on"), self.onman)
+            unitlist["web"].register((self.name,"set","ontimer"), self.ontimer)
+            unitlist["web"].register((self.name,"set","off"), self.offman)
+            unitlist["web"].register((self.name,"set","offtimer"), self.offtimer)
+            unitlist["web"].register((self.name,"set","auto"), self.noman)
+            unitlist["web"].register((self.name,"set","man"), self.man)
+            unitlist["web"].register((self.name,"get"), self.getstate)
+            
+
+    def setinput(self):
+        if self.inputop == "and":
+            self.state[0] = 1
+            for inp in self.ins:
+                self.state[0] = self.state[0] and inp.state[0]
+        else:
+            self.state[0] = 0
+            for inp in self.ins:
+                self.state[0] = self.state[0] or inp.state[0]
+
+    def setoutput(self):
+        self.out(self.state[3])
+                
+    def setman(self, req, qs):
+        self.state[1] = 1
+        return 0
+
+    def setnoman(self, req, qs):
+        self.state[1] = 0
+        self.state[2] = 0
         self.state[3] = self.state[0]
-    else:
-        self.state[3] = 0
+        self.setoutput()
+        return 0
 
-def offtimer(self):
-    self.state[2] = 1
-    self.state[3] = 0
-
-def offtimerend(self):
-    self.state[2] = 0
-    if self.state[1] == 0:
-        self.state[3] = self.state[0]
-    else:
+    def onman(self, req, qs):
+        self.state[2] = 0
         self.state[3] = 1
+        self.setoutput()
+        return 0
+
+    def offman(self, req, qs):
+        self.state[2] = 0
+        self.state[3] = 0
+        self.setoutput()
+        return 0
+
+    def autotrigchange(self):
+        self.setinput() # set self.state[0]
+        if self.state[1] == 0:
+            self.state[3] = self.state[0]
+            self.setoutput()
+
+    # def autotrigdrop(self):
+    #     self.state[0] = 0
+    #     if self.state[1] == 0:
+    #         self.state[3] = 0
+
+    def ontimer(self, req, qs):
+        self.state[2] = 1
+        self.state[3] = 1
+        self.setoutput()
+        return 0
+
+    def ontimerend(self):
+        self.state[2] = 0
+        if self.state[1] == 0:
+            self.state[3] = self.state[0]
+        else:
+            self.state[3] = 0
+        self.setoutput()
+
+    def offtimer(self, req, qs):
+        self.state[2] = 1
+        self.state[3] = 0
+        self.setoutput()
+        return 0
+
+    def offtimerend(self):
+        self.state[2] = 0
+        if self.state[1] == 0:
+            self.state[3] = self.state[0]
+        else:
+            self.state[3] = 1
+        self.setoutput()
 
--- src/multisensor.py
+++ src/multisensor.py
@@ -13,42 +13,49 @@ html = """<!DOCTYPE html>
 </html>
 """
 
-def req_decode(reqfull):
-    resource = []
-    querystring = None
-    try:
-        req = str(reqfull).split(" ")[1]
-        rq = req.split("?")
-        resource = rq[0].split("/")
-        if len(rq) > 1:
-            querystring = rq[1].split("&")
-    except:
-        pass
-    return resource, querystring
+class KNovaWebServer:
+    def __init__(self):
+        self.webhooks = []
 
-def http_loop(s):
-    cl, addr = s.accept()
-#    print('client connected from', addr)
-    ip = socket.inet_ntop(socket.AF_INET,addr[4:8]) # indovinato
-    print("request from "+ip)
-    cl_file = cl.makefile('rwb', 0)
-    req = cl_file.readline(1024)
-    res, qs = req_decode(req)
-    print(res)
-    print(qs)
-    while True:
-        line = cl_file.readline()
-        if not line or line == b'\r\n':
-            break
-    rows = ['<tr><td>%s</td><td>%s</td></tr>' % (str(p), pins[p]) for p in pins]
-    response = html % '\n'.join(rows)
-    cl.send(b'HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-    cl.send(bytes(response,"ascii"))
-    cl.close()
+    def register(self, req, callback):
+        self.webhooks.append((req, callback))
+
+    def req_decode(reqfull):
+        resource = []
+        querystring = None
+        try:
+            req = str(reqfull).split(" ")[1]
+            rq = req.split("?")
+            resource = rq[0].split("/")
+            if len(rq) > 1:
+                querystring = rq[1].split("&")
+        except:
+            pass
+        return resource, querystring
+
+    def http_loop(s):
+        cl, addr = s.accept()
+        #    print('client connected from', addr)
+        ip = socket.inet_ntop(socket.AF_INET,addr[4:8]) # indovinato
+        print("request from "+ip)
+        cl_file = cl.makefile('rwb', 0)
+        req = cl_file.readline(1024)
+        res, qs = req_decode(req)
+        print(res)
+        print(qs)
+        while True:
+            line = cl_file.readline()
+            if not line or line == b'\r\n':
+                break
+        rows = ['<tr><td>%s</td><td>%s</td></tr>' % (str(p), pins[p]) for p in pins]
+        response = html % '\n'.join(rows)
+        cl.send(b'HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        cl.send(bytes(response,"ascii"))
+        cl.close()
 
 
-def idle_loop():
-    print("idle loop")
+        def idle_loop():
+            print("idle loop")
 
 addr = socket.getaddrinfo('0.0.0.0', 8081)[0][-1]
 s = socket.socket()
