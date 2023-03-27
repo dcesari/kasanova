932d6e516eb9af4f691d48d2d415d208d7c9dad1
Davide Cesari <dcesari@arpae.it>
2023-03-27 15:51:23 +0200

    web server complete

--- sensorsmpy/multisensor.py
+++ sensorsmpy/multisensor.py
@@ -3,15 +3,6 @@
 import socket
 import select
 
-pins={1:'on', 2:'on', 3:'off'}
-html = """<!DOCTYPE html>
-<html>
-    <head> <title>ESP32 Pins</title> </head>
-    <body> <h1>ESP32 Pins</h1>
-        <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
-    </body>
-</html>
-"""
 
 htmle = """<!DOCTYPE html>
 <html>
@@ -21,6 +12,34 @@ htmle = """<!DOCTYPE html>
     </body>
 </html>
 """
+
+class WebServerRequest:
+    def __init__(self, method, resource, querydict, fp):
+        self.method = method
+        self.resource = resource
+        self.querydict = querydict
+        self.fp = fp
+
+
+    def senderror(self, code):
+        htcode = str(code)
+        try:
+            self.fp.send(b'HTTP/1.0 '+htcode+' OK\r\nContent-type: text/html\r\nConnection: close\r\n\r\n')
+            r = bytes(htmle % (htcode,),"ascii")
+            self.fp.send(r)
+            self.fp.close()
+        except:
+            pass
+
+
+    def sendresponse(self, ctype, cbody):
+        try:
+            self.fp.send(b'HTTP/1.0 200 OK\r\nContent-type: '+ctype+'\r\nConnection: close\r\n\r\n')
+            self.fp.send(bytes(cbody, "ascii"))
+            self.fp.close()
+        except:
+            pass
+
 
 class KNovaWebServer:
     def __init__(self, conf):
@@ -44,76 +63,86 @@ class KNovaWebServer:
     def register(self, req, callback):
         self.webhooks.append((req, callback))
 
-    def req_decode(self, reqfull):
-        meth = None
-        resource = []
-        querystring = None
+    def qs_to_dict(self, qs):
         querydict = {}
         try:
-            meth, req, proto = str(reqfull).split(" ")
-            rq = req.split("?")
-            resource = rq[0].split("/")
-            if len(rq) > 1:
-                querystring = rq[1].split("&")
-        except:
-            pass
-        if querystring is not None:
-            for el in querystring:
+            for el in qs.split("&"):
                 try:
                     k, v = el.split("=", 1)
                     querydict[k] = v
                 except:
                     querydict[el] = None
+        except:
+            pass
+        return querydict
+
+
+    def req_decode(self, reqfull):
+        meth = None
+        resource = []
+        querystring = None
+        querydict = {}
+        rq = None
+        try:
+            meth, req, proto = reqfull.decode().split(" ")
+            rq = req.split("?")
+            resource = rq[0].lstrip("/").split("/")
+            if len(rq) > 1:
+                querystring = rq[1].split("&")
+        except:
+            pass
+        if rq is not None:
+            if len(rq) > 1:
+                querydict = self.qs_to_dict(rq[1])
 
         return meth, resource, querydict
 
     def http_ready(self):
         cl, addr = self.sock.accept()
-        #    print('client connected from', addr)
         ip = socket.inet_ntop(socket.AF_INET,addr[4:8]) # indovinato
         print("request from "+ip)
         cl_file = cl.makefile('rwb', 0)
         req = cl_file.readline(1024)
         meth, res, qs = self.req_decode(req)
-        print(meth)
-        print(res)
-        print(qs)
         auth = False
+        length = None
+        # authorisation
         if self.allowip is not None:
             for i in self.allowip:
-                print(i)
-                print(ip)
                 if ip == i:
                     auth = True
         else:
             auth = True
-
+        # read headers
         while True:
-            line = cl_file.readline()
-            if not line or line == b'\r\n':
+            line = cl_file.readline(1024)
+            if not line or line == b"\r\n":
                 break
-        if not auth:
-            htcode = '400'
-            self.sendhtmlresponse(cl, htcode, htmle % (htcode,))
-            print(response)
-            return
-        rows = ['<tr><td>%s</td><td>%s</td></tr>' % (str(p), pins[p]) for p in pins]
-        htcode = '200'
-        self.sendhtmlresponse(cl, htcode, html % '\n'.join(rows))
-        #for h in self.webhooks:
-        #    if req == h[0]:
-        #        h[1]
-        #        return
+            try:
+                k, v = line.rstrip(b"\r\n").split(b" ")
+                if k == b"Content-Length:":
+                    length = int(v)
+            except:
+                pass
+        # read post data
+        if meth == "POST":
+            if length is not None:
+                postdata = cl_file.read(min(length,2048))
+                qs.update(self.qs_to_dict(postdata))
 
-    def sendhtmlresponse(self, cl, htcode, response):
-        try:
-            cl.send(b'HTTP/1.0 '+htcode+' OK\r\nContent-type: text/html\r\nConnection: close\r\n\r\n')
-            r = bytes(response,"ascii")
-            cl.send(r)
-            # cl.send(bytes(response,"ascii"))
-            cl.close()
-        except:
-            pass
+        request = WebServerRequest(meth, res, qs, cl)
+        # unauthorised
+        if not auth:
+            request.senderror(400)
+            return
+        for h in self.webhooks:
+            if res == h[0]:
+                # here OK, call callback
+                h[1](request)
+                return
+        # not found
+        request.senderror(404)
+
 
     def poll_loop(self):
         ready = self.httpoll.poll(1000)
@@ -125,7 +154,11 @@ class KNovaWebServer:
 
 
 if __name__ == '__main__':
+    def webcb(req):
+        req.sendresponse("text/json", '{"ciao": 88}')
+
     ws = KNovaWebServer({"allowip":'127.0.0.1'})
+    ws.register(["a","b"], webcb)
     ws.connect(None)
     while True:
         ws.poll_loop()
