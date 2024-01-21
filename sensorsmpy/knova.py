c4b7b0694ef63e32b2483df882ab9816bc72547c
Davide Cesari <dcesari@arpae.it>
2024-01-21 19:01:01 +0100

    partial integration of web server in main knova tool

--- sensorsmpy/knova.py
+++ sensorsmpy/knova.py
@@ -8,6 +8,8 @@ import time
 import ujson
 import network
 import ntptime
+import socket
+import select
 
 
 # tentative main loop, cb must return after a reasonable time
@@ -35,6 +37,8 @@ def KnovaDispatcher(conf):
     typ = conf.get("type", "")
     if conf["type"] == "wifinetwork":
         return KnovaWiFiNetwork(conf)
+    if conf["type"] == "webserver":
+        return KnovaWebServer(conf)
     if conf["type"] == "pushbutton":
         return KnovaPushButton(conf)
     if conf["type"] == "onoffbutton":
@@ -246,6 +250,119 @@ class KnovaWiFiNetwork(KnovaTool):
     def periodicupdate(self): # can i do something to stimulate connection?
         if self.nic.isconnected() and self.ntp:
             ntptime.settime()
+
+
+class KNovaWebServer(KnovaTool):
+    def __init__(self, conf):
+        super().__init__(conf)
+        self.webhooks = []
+        self.allowip = conf.get("allowip", None)
+        if type(self.allowip) == str:
+            self.allowip = [conf["allowip"]]
+        self.listenaddr = conf.get("listenaddr", "0.0.0.0")
+        self.port = conf.get("port", 8081)
+
+    def connect(self):
+        addr = socket.getaddrinfo(self.listenaddr, self.port)[0][-1]
+        self.sock = socket.socket()
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind(addr)
+        self.sock.listen(5)
+#        print('listening on', addr)
+        self.httpoll = select.poll()
+        self.httpoll.register(self.sock, select.POLLIN)
+
+    def register(self, req, callback):
+        self.webhooks.append((req, callback))
+
+    def qs_to_dict(self, qs):
+        querydict = {}
+        try:
+            for el in qs.split("&"):
+                try:
+                    k, v = el.split("=", 1)
+                    querydict[k] = v
+                except:
+                    querydict[el] = None
+        except:
+            pass
+        return querydict
+
+
+    def req_decode(self, reqfull):
+        meth = None
+        resource = []
+        querystring = None
+        querydict = {}
+        rq = None
+        try:
+            meth, req, proto = reqfull.decode().split(" ")
+            rq = req.split("?")
+            resource = rq[0].lstrip("/").rstrip("/").split("/")
+            if len(rq) > 1:
+                querystring = rq[1].split("&")
+        except:
+            pass
+        if rq is not None:
+            if len(rq) > 1:
+                querydict = self.qs_to_dict(rq[1])
+
+        return meth, resource, querydict
+
+    def http_ready(self):
+        cl, addr = self.sock.accept()
+        ip = socket.inet_ntop(socket.AF_INET,addr[4:8]) # indovinato
+#        print("request from "+ip)
+        cl_file = cl.makefile('rwb', 0)
+        req = cl_file.readline(1024)
+        meth, res, qs = self.req_decode(req)
+        auth = False
+        length = None
+        # authorisation
+        if self.allowip is not None:
+            for i in self.allowip:
+                if ip == i:
+                    auth = True
+        else:
+            auth = True
+        # read headers
+        while True:
+            line = cl_file.readline(1024)
+            if not line or line == b"\r\n":
+                break
+            try:
+                k, v = line.rstrip(b"\r\n").split(b" ")
+                if k == b"Content-Length:":
+                    length = int(v)
+            except:
+                pass
+        # read post data
+        if meth == "POST":
+            if length is not None:
+                postdata = cl_file.read(min(length,2048))
+                qs.update(self.qs_to_dict(postdata))
+
+        request = KnovaWebRequest(meth, res, qs, cl)
+        # unauthorised
+        if not auth:
+            request.senderror(400)
+            return
+        for h in self.webhooks:
+            if res == h[0]:
+                # here OK, call callback
+                h[1](request)
+                return
+        # not found
+        request.senderror(404)
+
+
+    def periodicupdate(self): # improve
+        ready = self.httpoll.poll(1000)
+        # handle errors in select here
+        if len(ready) > 0:
+            self.http_ready()
+            return 1
+        return 0
 
 
 # sensor/buttons tools
